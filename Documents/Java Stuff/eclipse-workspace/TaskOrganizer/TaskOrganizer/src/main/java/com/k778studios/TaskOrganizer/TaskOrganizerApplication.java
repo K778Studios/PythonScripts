41529a06ea2c92745e5f67bfd0c0fe641457d001
mdolinski <mdolinski@mdolinski-VirtualBox>
2018-11-01 19:59:12 -0400

    Simple test changes

--- Documents/Java Stuff/eclipse-workspace/TaskOrganizer/TaskOrganizer/src/main/java/com/k778studios/TaskOrganizer/TaskOrganizerApplication.java
+++ Documents/Java Stuff/eclipse-workspace/TaskOrganizer/TaskOrganizer/src/main/java/com/k778studios/TaskOrganizer/TaskOrganizerApplication.java
@@ -8,5 +8,6 @@ public class TaskOrganizerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskOrganizerApplication.class, args);
+		System.out.println("Hello World");
 	}
 }
